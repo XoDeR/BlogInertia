4246563f2a6a4bc993780b53b859d21d8951de72
XoDeR <XoDeR@users.noreply.github.com>
2024-10-12 23:20:30 +0200

    added posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        /*
+        $posts = Post::orderBy('created_at', 'desc')->with(['user', 'likes'])->paginate(20);
+
+        return view('posts.index', ['posts' => $posts]);
+        */
+    }
+
+    public function show(Post $post)
+    {
+        /*
+        return view('posts.show', ['post' => $post]);
+        */
+    }
+
+    public function store(Request $request)
+    {
+        /*
+        $validated = $request->validate([
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return back();
+        */
+    }
+
+    public function startEdit(Post $post)
+    {
+        /*
+        Gate::authorize('startEdit', $post);
+
+        return view('posts.edit', ['post' => $post]);
+        */
+    }
+
+    public function edit(Request $request, Post $post)
+    {
+        /*
+        Gate::authorize('edit', $post);
+
+        $validated = $request->validate([
+            'body' => 'required'
+        ]);
+
+        Post::find($post->id)->update([
+            'body' => $request->body
+        ]);
+        $post->refresh();
+
+        return redirect()->action([PostController::class, 'index']);
+        */
+    }
+
+    public function destroy(Post $post)
+    {
+        /*
+        Gate::authorize('delete', $post);
+
+        $post->delete();
+
+        return back();
+        */
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -22,6 +23,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/posts', [PostController::class, 'index'])->name('posts');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('/posts/edit/{post}', [PostController::class, 'startEdit'])->name('posts.startEdit');
+    Route::patch('/posts/edit/{post}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 });
 
 require __DIR__.'/auth.php';
