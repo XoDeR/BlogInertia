cb7c12243d282078e877d135015ad4de2685cf37
XoDeR <XoDeR@users.noreply.github.com>
2024-10-14 15:46:02 +0200

    removed unused autogenerated functions

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -14,30 +14,6 @@ class PostPolicy
     }
 
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Post $post): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Post $post): bool
@@ -51,21 +27,5 @@ class PostPolicy
     public function delete(User $user, Post $post): bool
     {
         return $this->getIsPostByUser($user, $post);
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Post $post): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Post $post): bool
-    {
-        //
     }
 }
